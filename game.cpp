abde125a5995da14ba0e4b8ac0bb88c66d561af8
KROTOKROT <konstantinv763@gmail.com>
2024-06-10 21:50:06 +0300

    Uraa

---
+++ game.cpp
@@ -0,0 +1,55 @@
+#include "game.h"
+#include <iostream>
+#include <fstream>
+#include <cstdlib> 
+
+Game::Game() : currentPlayer('X') {}
+
+void Game::switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+void Game::play() {
+    int row, col;
+    char winner = ' ';
+    while (winner == ' ' && !board.isFull()) {
+        board.display();
+        std::cout << "Player " << currentPlayer << "'s turn. Enter row and column: ";
+        std::cin >> row >> col;
+        if (board.placeMark(row, col, currentPlayer)) {
+            moves.push_back(std::make_pair(row, col));
+            winner = board.checkWinner();
+            switchPlayer();
+        }
+        else {
+            std::cout << "Invalid move! Try again.\n";
+        }
+    }
+    board.display();
+    if (winner == ' ')
+        std::cout << "It's a draw!\n";
+    else
+        std::cout << "Player " << winner << " wins!\n";
+    saveToFile(winner);
+    createExecutable();
+}
+
+void Game::saveToFile(char winner) {
+    std::ofstream file("game_results.txt", std::ios::app);
+    if (file.is_open()) {
+        file << "Winner: " << winner << "\n";
+        file << "Moves:\n";
+        for (const auto& move : moves) {
+            file << move.first << " " << move.second << "\n";
+        }
+        file << "-------------------\n";
+        file.close();
+    }
+    else {
+        std::cerr << "Unable to open file for writing!\n";
+    }
+}
+
+void Game::createExecutable() {
+    system("g++ main.cpp game.cpp board.cpp -o TicTacToe");
+}
---
+++ game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "board.h"
+#include <vector>
+
+class Game {
+private:
+    Board board;
+    char currentPlayer;
+    std::vector<std::pair<int, int>> moves;
+public:
+    Game();
+    void switchPlayer();
+    void play();
+    void saveToFile(char winner);
+    void createExecutable();
+};
+
+#endif
